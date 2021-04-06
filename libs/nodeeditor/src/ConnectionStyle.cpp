20549a4f2d3ca90111905ca8e08dfa8568c4a530
abramcumner <abramcumner@yandex.ru>
2021-04-06 22:09:35 +0300

    Update nodeeditor to Qt6

--- libs/nodeeditor/src/ConnectionStyle.cpp
+++ libs/nodeeditor/src/ConnectionStyle.cpp
@@ -7,6 +7,7 @@
 #include <QtCore/QJsonObject>
 #include <QtCore/QJsonValueRef>
 #include <QtCore/QJsonArray>
+#include <QtCore/QRandomGenerator>
 
 #include <QDebug>
 
@@ -157,6 +158,7 @@ normalColor() const
   return NormalColor;
 }
 
+QRandomGenerator random;
 
 QColor
 ConnectionStyle::
@@ -166,8 +168,8 @@ normalColor(QString typeId) const
 
   std::size_t const hue_range = 0xFF;
 
-  qsrand(hash);
-  std::size_t hue = qrand() % hue_range;
+  random.seed(hash);
+  std::size_t hue = random.generate() % hue_range;
 
   std::size_t sat = 120 + hash % 129;
 
